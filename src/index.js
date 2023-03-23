897be81c787ee279c2765e1a3218ba65b384b345
Drikadev <126597972+drika-dev@users.noreply.github.com>
2023-03-23 01:47:34 -0300

    calculadora

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+reportWebVitals();
+// é um conjunto de web vitals que pode ser aplicado em todas as páginas do site
